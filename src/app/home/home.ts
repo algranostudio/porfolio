4c48d7d952a9284f2f908fe367814d2c7c4c6228
rosana <rosmarielnunez@gmail.com>
2026-04-06 18:04:52 +0200

    Update-component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -28,6 +28,47 @@ export class Home implements AfterViewInit {
    @ViewChild('scrollContainer') scrollContainer!: ElementRef<HTMLDivElement>;
   @ViewChild('cursor') cursorRef!: ElementRef<HTMLDivElement>;
 
+  currentSlide = 0;
+
+  testimonials = [
+    {
+      text: 'El nuevo panel se ve mucho mejor y es mucho más fácil encontrar lo que necesito. Ahora puedo dedicar menos tiempo a tareas administrativas y más tiempo con las actividades de mis alumnos.',
+      avatar: '/images/avatar/avatar_rex_academy.png',
+      avatarAlt: 'Logo REX Academy',
+      role: 'CS Teacher',
+      company: 'REX Academy'
+    },
+    {
+      text: 'La nueva experiencia digital hizo que nuestra marca se sintiera más cercana y humana. Ahora podemos comunicar información compleja de una forma sencilla y visualmente atractiva, mejorando la percepción y la relación con nuestros clientes.',
+      avatar: '/images/avatar/avatar_nikko_1.png',
+      avatarAlt: 'Logo Nikko AM',
+      role: 'Equipo de Marketing',
+      company: 'Nikko AM'
+    },
+    {
+      text: 'El nuevo diseño del menú cambió por completo la experiencia. Los clientes entienden todo de un vistazo, eligen con más confianza y los camareros trabajan con más calma y rapidez.',
+      avatar: '/images/avatar/avatar_pan_de_queso.png',
+      avatarAlt: 'La Casa Pan de Queso',
+      role: 'Ademir, Propietario',
+      company: 'La Casa del Pan de Queso'
+    },
+    {
+      text: 'Pensaba que nuestro sitio funcionaba bien… hasta que Al Grano nos mostró todo lo que estábamos pasando por alto. Ahora sentimos que todo fluye mejor, y eso se nota en el negocio.',
+      avatar: '/images/avatar/avatar_Yulia_1.png',
+      avatarAlt: 'Yulia',
+      role: 'Yulia, Propietaria',
+      company: 'Baby Boom NSK'
+    }
+  ];
+
+  nextSlide(): void {
+    this.currentSlide = (this.currentSlide + 1) % this.testimonials.length;
+  }
+
+  prevSlide(): void {
+    this.currentSlide = (this.currentSlide - 1 + this.testimonials.length) % this.testimonials.length;
+  }
+
   ngAfterViewInit(): void {
     const container = this.scrollContainer.nativeElement;
 
@@ -73,4 +114,6 @@ export class Home implements AfterViewInit {
       behavior: 'smooth'
     });
   }
+
+
 }
